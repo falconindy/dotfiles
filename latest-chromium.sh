52e62a7a8f99d5c55450465735ac2afb5547eaae
Dave Reisner <haruko@quake.localdomain>
2009-12-03 18:05:41 -0500

    Revert "Merge branch 'A'"
    This reverts commit 787a0fbd2c5ef4a5b05bcf4e896888020470b9b9, reversing
    changes made to 841cef779ec39ee7d58414c75c5ffa39d964fa5e.

--- latest-chromium.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-[[ "$1" = "yes" ]] && yaourt -S chromium-browser-bin && exit
-
-echo "Currently using build: `pacman -Qi chromium-browser-bin | grep Version | cut -d: -f2`"
-echo "Latest build available: `curl --silent http://build.chromium.org/buildbot/snapshots/chromium-rel-linux-64/LATEST`"
--- pokeDynDNS.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-USERNAME=falconindy
-PASSWORD=nightmare
-IP=`curl http://www.whatismyip.com/automation/n09230945.asp`
-
-curl -v -k "http://$USERNAME:$PASSWORD@members.dyndns.org/nic/update?hostname=notrly.mine.nu&myip=$IP&wildcard=NOCHG&mx=NOCHG&backmx=NOCHG"
