e37118b8cb454233e13f85b8706bb334aa61e015
Dave Reisner <d@falconindy.com>
2010-01-17 14:22:57 -0500

    Initial commit

--- .irssi/scripts/notify.pl
+++ .irssi/scripts/notify.pl
@@ -20,7 +20,7 @@ $VERSION = "0.01";
 );
 
 Irssi::settings_add_str('notify', 'notify_icon', 'gtk-dialog-info');
-Irssi::settings_add_str('notify', 'notify_time', '5000');
+Irssi::settings_add_str('notify', 'notify_time', '1000');
 
 sub notify {
     my ($server, $summary, $message) = @_;
