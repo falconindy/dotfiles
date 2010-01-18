a3eb27ac500e9ff73eba61d379aedc2b7715d640
Dave Reisner <d@falconindy.com>
2010-01-18 15:30:36 -0500

    df can take bashex without needing to use ls subshell

--- .conky/hddmon.sh
+++ .conky/hddmon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Print any mount points that have passed 60% capacity
 
-df -h $(ls /dev/sd[a-z][0-9]) | tail --lines=+2 | awk '{split($5,a,"%"); if (a[1] > 60) printf " [ %s: %s ]", $6, $5;}'
+df -h /dev/sd[a-z][0-9] | tail --lines=+2 | awk '{split($5,a,"%"); if (a[1] > 60) printf " [ %s: %s ]", $6, $5;}'
