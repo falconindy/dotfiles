3e2ec3b8103cf578468ec0eecd6b9edaaa1dfdc2
Dave Reisner <d@falconindy.com>
2010-01-23 12:52:13 -0500

    Truncate track name if longer than supplied

--- .conky/nowPlaying.sh
+++ .conky/nowPlaying.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 track=$(mpc current)
+maxlength=50
+if [[ ${#track} -gt $maxlength ]]; then
+    track=${track:0:$maxlength}...
+fi
+
 if [[ "!$track" != "!" ]]; then
     echo "[ $track ] "
 fi
