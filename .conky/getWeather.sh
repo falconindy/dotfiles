7882d97adf05c784a374124e16d91622e1464ee7
Dave Reisner <d@falconindy.com>
2010-04-04 10:17:23 -0400

    Fix sed filter

--- .conky/getWeather.sh
+++ .conky/getWeather.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo $(curl --silent 'http://weather.yahooapis.com/forecastrss?w=12760842' | grep -A1 "Current Conditions" | sed -n 's|[A-Za-z]*, \([0-9]*\) F<.*|\1°F|p')
+echo $(curl --silent 'http://weather.yahooapis.com/forecastrss?w=12760842' |\
+   grep -A1 "Current Conditions" |\
+  sed -n 's|.*, \([0-9]*\) F<.*|\1°F|p')
