1fc28af513035d826c3ac4b0c1c19e7ccc1dbb00
Dave Reisner <d@falconindy.com>
2010-04-06 16:01:22 -0400

    fix spacing

--- .conky/getWeather.sh
+++ .conky/getWeather.sh
@@ -2,4 +2,4 @@
 
 echo $(curl --silent 'http://weather.yahooapis.com/forecastrss?w=12760842' |\
    grep -A1 "Current Conditions" |\
-  sed -n 's|.*, \([0-9]*\) F<.*|\1°F|p')
+   sed -n 's|.*, \([0-9]*\) F<.*|\1°F|p')
