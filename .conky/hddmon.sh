ea8d750c781699218532ba18068e6879fd393afc
Dave Reisner <d@falconindy.com>
2010-04-23 15:53:21 -0400

    Simplify logic, removing a pipe

--- .conky/hddmon.sh
+++ .conky/hddmon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Print any mount points that have passed 60% capacity
 
-df -h /dev/sd[a-z][0-9] | tail --lines=+2 | awk '{split($5,a,"%"); if (a[1] > 60) printf " [ %s: %s ]", $6, $5;}'
+df | awk '/^\/dev\/sd/{split($5,a,"%"); if (a[1] > 60) printf " [ %s: %s ]", $6, $5;}'
