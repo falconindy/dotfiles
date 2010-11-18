dd321f7315e3d4a9fa6cc17c2b54b3c44587a6c0
Dave Reisner <d@falconindy.com>
2010-11-18 15:27:56 -0500

    getWeather: simplify using xmllint

--- .config/conky/getWeather.sh
+++ .config/conky/getWeather.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-curl --silent 'http://weather.yahooapis.com/forecastrss?w=12760842' |\
-   grep -A1 "Current Conditions" |\
-   sed -n 's|.*, \([0-9]*\) F<.*|\1°F|p'
+eval $(curl -s 'http://weather.yahooapis.com/forecastrss?w=12760842' | xmllint --xpath '//*/@temp' -)
+echo "$temp°F"
