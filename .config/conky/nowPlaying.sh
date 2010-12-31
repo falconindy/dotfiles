b8d8ca6268a623cef045b0c27373b2e28f71225d
Dave Reisner <d@falconindy.com>
2010-12-31 09:49:16 -0500

    conky/nowPlaying: use socat instead of mpc

--- .config/conky/nowPlaying.sh
+++ .config/conky/nowPlaying.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+shopt -s extglob
+
 MAXLEN=50
 
-read track < <(mpc current)
+mpd() {
+  socat - tcp-connect:localhost:6600
+}
 
-# Truncate if longer than maxlength
+while IFS=':' read attr val; do
+  case "$attr" in
+    Artist) artist="${val:1}";;
+    Title) title="${val:1}" ;;
+  esac
+done < <(mpd <<< "currentsong")
+
+track="$artist - $title"
+
 if (( ${#track} > MAXLEN )); then
   truncated=${track:0:MAXLEN}
   track=${truncated% *}...
 fi
 
-# Display
 [[ -n $track ]] && echo "[ $track ] "
+
